54b848bc1c6cf713677f609215dcd15d0feb7798
Henry Young <henry.young@industrieit.com>
2017-07-17 15:13:32 +1000

    Fix ruby issue with regex/string match in 2.4

--- lib/featureflow/feature.rb
+++ lib/featureflow/feature.rb
@@ -2,7 +2,7 @@ module Featureflow
   class Feature
     def self.create(key, failover_variant = 'off')
       raise ArgumentError, 'Parameter key must be a String' unless valid_key?(key)
-      raise ArgumentError, 'Parameter default_variant must be a String' unless valid_key?(failover_variant) || failover_variant.is_a?(NilClass)
+      raise ArgumentError, 'Parameter failover_variant must be a String containing only letters, numbers, hyphens or underscores' unless valid_key?(failover_variant) || failover_variant.is_a?(NilClass)
       {
           key: key,
           failover_variant: failover_variant,
@@ -20,5 +20,5 @@ module Featureflow
 end
 
 def valid_key?(value)
-  value.is_a?(String) && !value.empty? && /^[a-z\-\_0-9]+$/.match?(value)
+  value.is_a?(String) && !value.empty? &&  value =~ /^[a-z\-\_0-9]+$/
 end
--- lib/featureflow/version.rb
+++ lib/featureflow/version.rb
@@ -1,3 +1,3 @@
 module Featureflow
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
